22ef1d188023f63584ae26d9353c9c3fef70f6d3
Shengjun Xia <shengjun.xia@grabtaxi.com>
2023-10-18 10:08:19 +0800

    Initial Commit

---
+++ EssentailFeed/EssentailFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentailFeed.h
+//  EssentailFeed
+//
+//  Created by Shengjun Xia on 2023/10/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentailFeed.
+FOUNDATION_EXPORT double EssentailFeedVersionNumber;
+
+//! Project version string for EssentailFeed.
+FOUNDATION_EXPORT const unsigned char EssentailFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentailFeed/PublicHeader.h>
+
+
