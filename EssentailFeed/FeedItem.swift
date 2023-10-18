cb42698b6df39b37943c11574bcc8c91c99fd364
Shengjun Xia <shengjun.xia@grabtaxi.com>
2023-10-18 10:09:40 +0800

    Create project and the feature model

---
+++ EssentailFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentailFeed
+//
+//  Created by Shengjun Xia on 2023/10/18.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
