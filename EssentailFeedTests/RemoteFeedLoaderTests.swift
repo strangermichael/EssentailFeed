0342f7530629121d1da98ff5786d872d9df15257
Shengjun Xia <shengjun.xia@grabtaxi.com>
2023-12-03 11:11:32 +0800

    test feed loader receive error when client fail

--- EssentailFeedTests/RemoteFeedLoaderTests.swift
+++ EssentailFeedTests/RemoteFeedLoaderTests.swift
@@ -39,6 +39,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
     XCTAssertEqual(client.requestedURLs, [url, url])
   }
   
+  func test_load_deliverErrorOnClientError() {
+    let (sut, client) = makeSUT()
+    client.error = NSError(domain: "Test", code: 0)
+    var capturedErrors: [RemoteFeedLoader.Error] = []
+    sut.load {
+      capturedErrors.append($0)
+    }
+    XCTAssertEqual(capturedErrors, [.connectivity])
+  }
+  
   //MARK: - Helper
   func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
     let client = HTTPClientSpy()
@@ -48,9 +58,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
   
   class HTTPClientSpy: HTTPClient {
     var requestedURLs: [URL] = []
+    var error: Error?
     
-    func get(from url: URL) {
+    func get(from url: URL, completion: @escaping (Error) -> Void) {
       requestedURLs.append(url)
+      if let error = error {
+        completion(error)
+      }
     }
   }
 }
