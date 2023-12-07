4f2b167439e15a110acf7116d4a0432bcddaf378
Shengjun Xia <shengjun.xia@grabtaxi.com>
2023-12-07 10:59:20 +0800

    move out creation to factory method to avoid build process code change to break test code

--- EssentailFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentailFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -45,7 +45,7 @@ class URLSessionHTTPClientTests: XCTestCase {
       XCTAssertEqual(receivedRequest.httpMethod, "GET")
       exp.fulfill()
     }
-    URLSessionHTTPClient().get(from: url, completion: { _ in })
+    makeSUT().get(from: url, completion: { _ in })
     wait(for: [exp], timeout: 1.0)
   }
   
@@ -54,7 +54,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     let error = NSError(domain: "any error", code: 1)
     URLProtocolStub.stub(data: nil, response: nil, error: error)
     
-    let sut = URLSessionHTTPClient()
+    let sut = makeSUT()
     let exp = expectation(description: "wait for completion")
     sut.get(from: url) { result in
       switch result {
@@ -71,6 +71,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     wait(for: [exp], timeout: 1.0)
   }
   
+  func makeSUT() -> URLSessionHTTPClient {
+    URLSessionHTTPClient()
+  }
   
   class URLProtocolStub: URLProtocol {
     private static var stub: Stub?
