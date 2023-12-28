01a318fe9c459b3edc6a63f0ef2dbc60060980f0
Shengjun Xia <shengjun.xia@grabtaxi.com>
2023-12-28 10:03:00 +0800

    save command request cache deletion

--- EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,26 +6,48 @@
 //
 
 import XCTest
+import EssentailFeed
 
 class LocalFeedLoader {
   private let store: FeedStore
   init(store: FeedStore) {
     self.store = store
   }
+  
+  func save(items: [FeedItem]) {
+    store.deleteCachedFeed()
+  }
 }
 
 class FeedStore {
   var deleteCacheFeedCallCount = 0
   
+  func deleteCachedFeed() {
+    deleteCacheFeedCallCount += 1
+  }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
   
   func test_init_doesNotDeleteCacheUponCreation() {
     let store = FeedStore()
-    let sut = LocalFeedLoader(store: store)
+    _ = LocalFeedLoader(store: store)
     XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
   }
   
+  func test_save_requestCacheDeletion() {
+    let store = FeedStore()
+    let sut = LocalFeedLoader(store: store)
+    let items = [uniqueItem(), uniqueItem()]
+    sut.save(items: items)
+    XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+  }
   
+  private func uniqueItem() -> FeedItem {
+    FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+  }
+  
+  func anyURL() -> URL {
+    URL(string: "http://url.com")!
+  }
 }
