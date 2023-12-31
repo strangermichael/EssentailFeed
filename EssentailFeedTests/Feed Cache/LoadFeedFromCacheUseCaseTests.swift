a8591b760d61aab661f52ec9155e00d01f4502db
Shengjun Xia <shengjun.xia@grabtaxi.com>
2023-12-31 17:17:15 +0800

    load delivers no images on more than seven days old cache

--- EssentailFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentailFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -56,6 +56,16 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
     }
   }
   
+  func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+    let fixedCurrentDate = Date()
+    let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+    let feed = uniqueImageFeed()
+    let moreThansevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+    expect(sut, toCompletWith: .success([])) {
+      store.complelteRetrieval(with: feed.local, timestamp: moreThansevenDaysOldTimeStamp)
+    }
+  }
+  
   //Helper
   private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
     let store = FeedStoreSpy()
