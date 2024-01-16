aa3da150b2a44eb6151c42c4c4344fbe5073c438
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-01-16 09:17:14 +0800

    Add comments for HTTPClient to tell clients are responsible for dispatch result to appropriate thread

--- EssentailFeed/Feed API/HTTPClient.swift
+++ EssentailFeed/Feed API/HTTPClient.swift
@@ -14,5 +14,7 @@ public enum HTTPClientResult {
 
 public protocol HTTPClient {
   //shouldn't use RemoteFeedLoader.Error here, since it's domain error
+  ///The completion handler can be invoked in any thread
+  ///Clients are responsible to dispacth to appropriate thread
   func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
