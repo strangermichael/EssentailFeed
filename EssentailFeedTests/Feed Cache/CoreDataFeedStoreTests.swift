07eeba706d73a16637cff11e68390656848aed64
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-01-19 09:37:55 +0800

    Create empty core data feed store test

---
+++ EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,84 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentailFeedTests
+//
+//  Created by Shengjun Xia on 2024/1/19.
+//
+
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpec {
+  func test_retrieve_deliversEmptyOnEmptyCache() {
+    
+  }
+  
+  func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+    
+  }
+  
+  func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+    
+  }
+  
+  func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+    
+  }
+  
+  func test_retrieve_deliversFailureOnRetrievalError() {
+    
+  }
+  
+  func test_retrieve_hasNoSideEffectsOnFailure() {
+    
+  }
+  
+  func test_insert_overridesPreviouslyInsertedCacheValues() {
+    
+  }
+  
+  func test_insert_deliversNoErrorOnEmptyCache() {
+    
+  }
+  
+  func test_insert_deliversNoErrorOnNonEmptyCache() {
+    
+  }
+  
+  func test_insert_deliversErrorOnInsertionError() {
+    
+  }
+  
+  func test_insert_hasNoSideEffectsOnInsertionError() {
+    
+  }
+  
+  func test_delete_deliversNoErrorOnEmptyCache() {
+    
+  }
+  
+  func test_delete_hasNoSideEffectsOnEmptyCache() {
+    
+  }
+  
+  func test_delete_deliversNoErrorOnNonEmptyCache() {
+    
+  }
+  
+  func test_delete_emptiesPreviouslyInsertedCached() {
+    
+  }
+  
+  func test_delete_deliversErrorOnDeletionError() {
+    
+  }
+  
+  func test_delete_hasNoSideEffectsOnDeletionError() {
+    
+  }
+  
+  
+  func test_storeSideEffects_runSerially() {
+    
+  }
+  
+}
