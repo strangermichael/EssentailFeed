a7c738c8db84b459919ad34169bd65f37eb04164
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-01-19 09:42:40 +0800

    test_retrieve_hasNoSideEffectsOnEmptyCache

--- EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -29,12 +29,13 @@ class CoreDataFeedStore: FeedStore {
 final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpec {
   
   func test_retrieve_deliversEmptyOnEmptyCache() {
-    let sut = CoreDataFeedStore()
+    let sut = makeSUT()
     assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
   }
   
   func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-    
+    let sut = makeSUT()
+    assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
   }
   
   func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
@@ -102,4 +103,10 @@ final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpec {
     
   }
   
+  //MARK: - helper
+  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+    let sut = CoreDataFeedStore()
+    trackForMemoryLeaks(sut, file: file, line: line)
+    return sut
+  }
 }
