9c87960481517e7cd03ffcc2bb3381f1dff2c360
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-01-20 12:07:30 +0800

    Create EssentailFeedCacheIntegrationTests to separate from fast unit test

---
+++ EssentailFeedCacheIntegrationTests/EssentailFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentailFeedCacheIntegrationTests.swift
+//  EssentailFeedCacheIntegrationTests
+//
+//  Created by Shengjun Xia on 2024/1/20.
+//
+
+import XCTest
+
+final class EssentailFeedCacheIntegrationTests: XCTestCase {
+  
+}
