83430628d8ae6107118d9af9b10c82958cc424b7
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-02-24 09:10:18 +0800

    Move Feed VC creation logic to factory method

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,13 +12,20 @@ public final class FeedUIComposer {
   private init() {}
   public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
     let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
+    let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
+    let presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader), loadingView: WeakRefVirtualProxy(feedController))
+    presentationAdapter.presenter = presenter
+    return feedController
+  }
+}
+
+private extension FeedViewController {
+  static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
     let bundle = Bundle(for: FeedViewController.self)
     let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
     let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-    feedController.delegate = presentationAdapter
-    let presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader), loadingView: WeakRefVirtualProxy(feedController))
-    presentationAdapter.presenter = presenter
-    feedController.title = FeedPresenter.title
+    feedController.delegate = delegate
+    feedController.title = title
     return feedController
   }
 }
