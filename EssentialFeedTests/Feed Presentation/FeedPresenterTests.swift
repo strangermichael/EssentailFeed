6f6c91eff8b9699c516b6f3b7a69c3b4ed512208
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-10 09:13:55 +0800

    Feedpresenter start loading on didStartLoadingFeed

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,6 +7,14 @@
 
 import XCTest
 
+struct FeedLoadingViewModel {
+  let isLoading: Bool
+}
+
+protocol FeedLoadingView {
+  func display(viewModel: FeedLoadingViewModel)
+}
+
 protocol FeedErrorView {
   func display(_ viewModel: FeedErrorViewModel)
 }
@@ -24,14 +32,17 @@ struct FeedErrorViewModel {
 }
 
 class FeedPresenter {
+  private let loadingView: FeedLoadingView
   private let errorView: FeedErrorView
   
-  init(view: FeedErrorView) {
-    self.errorView = view
+  init(loadingView: FeedLoadingView, errorView: FeedErrorView) {
+    self.loadingView = loadingView
+    self.errorView = errorView
   }
   
   func didStartLoadingFeed() {
     errorView.display(.noError)
+    loadingView.display(viewModel: FeedLoadingViewModel(isLoading: true))
   }
 }
 
@@ -42,31 +53,36 @@ class FeedPresenterTests: XCTestCase {
     XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
   }
   
-  func test_didStartLoadingFeed_displayNoErrorMessage() {
+  func test_didStartLoadingFeed_displayNoErrorMessageAndStartsLoading() {
     let (sut, view) = makeSUT()
     sut.didStartLoadingFeed()
-    XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    XCTAssertEqual(view.messages, [.display(errorMessage: .none), .display(isLoading: true)])
   }
   
   //MARK: - Helpers
   private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
     let view = ViewSpy()
-    let sut = FeedPresenter(view: view)
+    let sut = FeedPresenter(loadingView: view, errorView: view)
     trackForMemoryLeaks(view, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, view)
   }
   
   
-  private class ViewSpy: FeedErrorView {
+  private class ViewSpy: FeedErrorView, FeedLoadingView {
     enum Message: Equatable {
       case display(errorMessage: String?)
+      case display(isLoading: Bool)
     }
     
     private(set) var messages: [Message] = []
     
     func display(_ viewModel: FeedErrorViewModel) {
       messages.append(.display(errorMessage: viewModel.message))
+    }
+    
+    func display(viewModel: FeedLoadingViewModel) {
+      messages.append(.display(isLoading: viewModel.isLoading))
     }
     
   }
