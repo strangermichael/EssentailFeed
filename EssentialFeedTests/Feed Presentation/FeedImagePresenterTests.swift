ec986f3d25f4547c22a80191f09cdec906c6ecb3
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-10 10:19:47 +0800

    test_didStartLoadingImageData_displayNoImageAndShowLoadingAndShowRetry

---
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -0,0 +1,97 @@
+//
+//  FeedImagePresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Shengjun Xia on 2024/3/10.
+//
+
+import XCTest
+import EssentialFeed
+
+protocol FeedImageView {
+  associatedtype Image
+  
+  func display(_ model: FeedImageViewModel<Image>)
+}
+
+final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+  private let view: View
+  
+  init(view: View) {
+    self.view = view
+  }
+  
+  func didStartLoadingImageData(for model: FeedImage) {
+    view.display(FeedImageViewModel(
+      description: model.description,
+      location: model.location,
+      image: nil,
+      isLoading: true,
+      shouldRetry: false))  }
+}
+
+struct FeedImageViewModel<Image> {
+  let description: String?
+  let location: String?
+  let image: Image?
+  let isLoading: Bool
+  let shouldRetry: Bool
+  
+  var hasLocation: Bool {
+    return location != nil
+  }
+}
+
+final class FeedImagePresenterTests: XCTestCase {
+  
+  func test_init_doesNotSendAnyMessages() {
+    let (_, view) = makeSUT()
+    XCTAssertEqual(view.messages, [])
+  }
+  
+  
+  func test_didStartLoadingImageData_displayNoImageAndShowLoadingAndShowRetry() {
+    let (sut, view) = makeSUT()
+    let image = uniqueImage()
+    sut.didStartLoadingImageData(for: image)
+    XCTAssertEqual(view.messages, [.display(image: nil),
+                                   .display(location: image.location),
+                                   .display(description: image.description),
+                                   .display(isLoading: true),
+                                   .display(shouldRetry: false)
+                                  ])
+  }
+  
+  //MARK: - helper
+  private func makeSUT() -> (FeedImagePresenter<ViewSpy, AnyImage>, ViewSpy) {
+    let view = ViewSpy()
+    let presenter = FeedImagePresenter<ViewSpy, AnyImage>(view: view)
+    return (presenter, view)
+  }
+  
+}
+
+struct AnyImage: Hashable { }
+
+
+class ViewSpy: FeedImageView {
+  enum Message: Hashable {
+    case display(image: Image?)
+    case display(isLoading: Bool)
+    case display(shouldRetry: Bool)
+    case display(description: String?)
+    case display(location: String?)
+  }
+  
+  typealias Image = AnyImage
+  
+  private(set) var messages: Set<Message> = []
+  
+  func display(_ model: FeedImageViewModel<Image>) {
+    messages.insert(.display(image: model.image))
+    messages.insert(.display(isLoading: model.isLoading))
+    messages.insert(.display(shouldRetry: model.shouldRetry))
+    messages.insert(.display(description: model.description))
+    messages.insert(.display(location: model.location))
+  }
+}
