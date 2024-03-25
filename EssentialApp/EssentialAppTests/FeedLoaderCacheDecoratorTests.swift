908e3c01f26f5d96660523eb84ce5aef3b7448be
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-25 08:29:54 +0800

    Create cache protocol

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -8,10 +8,16 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedCache {
+  typealias SaveResult = Error?
+  
+  func save(items: [FeedImage], completion: @escaping (SaveResult) -> Void)
+}
+
 class FeedLoaderCacheDecorator: FeedLoader {
   private let decoratee: FeedLoader
   
-  init(decoratee: FeedLoader) {
+  init(decoratee: FeedLoader, cache: FeedCache) {
     self.decoratee = decoratee
   }
   
@@ -33,11 +39,32 @@ final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     expect(sut, toCompleteWith: .failure(anyNSError()))
   }
   
-  private func makeSUT(loaderResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
+  func test_load_cachesLoadedFeedOnLoaderSuccess() {
+    let cache = CacheSpy()
+    let feed = uniqueFeed()
+    let sut = makeSUT(loaderResult: .success(feed), cache: cache)
+    sut.load { _ in }
+    XCTAssertEqual(cache.messages, [.save(feed)])
+  }
+  
+  private func makeSUT(loaderResult: FeedLoader.Result, cache: CacheSpy = .init(), file: StaticString = #file, line: UInt = #line) -> FeedLoader {
     let loader = FeedLoaderStub(result: loaderResult)
-    let sut = FeedLoaderCacheDecorator(decoratee: loader)
+    let sut = FeedLoaderCacheDecorator(decoratee: loader, cache: cache)
     trackForMemoryLeaks(loader)
     trackForMemoryLeaks(sut)
     return sut
+  }
+  
+  private class CacheSpy: FeedCache {
+    private(set) var messages: [Message] = []
+    
+    enum Message: Equatable {
+      case save([FeedImage])
+    }
+    
+    func save(items: [FeedImage], completion: @escaping (SaveResult) -> Void) {
+      messages.append(.save(items))
+      completion(nil)
+    }
   }
 }
