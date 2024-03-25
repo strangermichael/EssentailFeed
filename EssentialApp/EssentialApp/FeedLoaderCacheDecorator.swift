0560df3a481ad58f56429e6eabe0ceaef524b3b6
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-25 08:49:40 +0800

    use a private extension to make code more clear

--- EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -20,9 +20,15 @@ public class FeedLoaderCacheDecorator: FeedLoader {
   public func load(completion: @escaping (FeedLoader.Result) -> Void) {
     decoratee.load { [weak self] result in
       completion(result.map({ feed in
-        self?.cache.save(items: feed, completion: { _ in })
+        self?.cache.saveIgnoringResult(feed)
         return feed
       }))
     }
+  }
+}
+
+private extension FeedCache {
+  func saveIgnoringResult(_ feed: [FeedImage]) {
+    save(items: feed, completion: { _ in })
   }
 }
