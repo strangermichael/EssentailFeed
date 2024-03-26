dbdfd1528865a79b0fabae8bee987b09e420f6ec
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-26 08:27:34 +0800

    test_onlaunch_displaysRemoteFeedWhenCustomerHasConnectivity

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,34 +8,15 @@
 import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+  
+  func test_onlaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+    let app = XCUIApplication()
+    app.launch()
+    app.waitForExistence(timeout: 5) //有时候接口没完成 或者 cell不可见 这个时候等一会儿确保UI都有
+    let feedCells = app.cells.matching(identifier: "feed-image-cell")
+    XCTAssertEqual(feedCells.count, 22)
+    let appImage = app.images.matching(identifier: "feed-image-view").firstMatch
+    XCTAssertTrue(appImage.exists)
+  }
+  
 }
