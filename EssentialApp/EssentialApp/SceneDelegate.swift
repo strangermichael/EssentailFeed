bc5ba4e38eef66f131d952490c4dff2776161c55
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-28 08:59:51 +0800

    test_sceneWillConnectToSession_configuresRootViewController

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -15,6 +15,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
 
+  private lazy var httpClient: HTTPClient = {
+    URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+  }()
+  
+  private lazy var store: FeedStore & FeedImageDataStore = {
+    try! CoreDataFeedStore(storeURL: localStoreURL, bundle: Bundle(for: CoreDataFeedStore.self))
+  }()
+  
+  convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
+    self.init()
+    self.httpClient = httpClient
+    self.store = store
+  }
+  
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -29,9 +43,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     let client = makeRemoteClient()
     let remoteFeedLoader = RemoteFeedLoader(client: client, url: remoteURL)
     let remoteImageLoader = RemoteFeedImageDataLoader(client: client)
-    let localStore = try! CoreDataFeedStore(storeURL: localStoreURL, bundle: Bundle(for: CoreDataFeedStore.self))
-    let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-    let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+    let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
+    let localImageLoader = LocalFeedImageDataLoader(store: store)
     
     let feedViewController = FeedUIComposer.feedComposedWith(feedLoader:
                                                               FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader),
@@ -42,7 +55,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   func makeRemoteClient() -> HTTPClient {
-    URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    httpClient
   }
   
   func sceneDidDisconnect(_ scene: UIScene) {
---
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -0,0 +1,119 @@
+//
+//  FeedAcceptanceTests.swift
+//  EssentialAppTests
+//
+//  Created by Shengjun Xia on 2024/3/28.
+//
+
+import XCTest
+import EssentialFeed
+import EssentialFeediOS
+@testable import EssentialApp
+
+final class FeedAcceptanceTests: XCTestCase {
+  
+  func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+    let store = InMemoryFeedStore.empty
+    let httpClient = HTTPClientStub.online(response)
+    let feed = launch(httpClient: httpClient, store: store)
+    XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
+    XCTAssertEqual(feed.simulateFeedImageViewVisible(at: 0)?.renderedImage, makeImageData())
+    XCTAssertEqual(feed.simulateFeedImageViewVisible(at: 1)?.renderedImage, makeImageData())
+  }
+  
+  private func launch(
+    httpClient: HTTPClientStub = .offline,
+    store: InMemoryFeedStore = .empty
+  ) -> FeedViewController {
+    let sut = SceneDelegate(httpClient: httpClient, store: store)
+    sut.window = UIWindow()
+    sut.configureWindow()
+    
+    let nav = sut.window?.rootViewController as? UINavigationController
+    return nav?.topViewController as! FeedViewController
+  }
+  
+  private class HTTPClientStub: HTTPClient {
+    private class Task: HTTPClientTask {
+      func cancel() {}
+    }
+    
+    private let stub: (URL) -> HTTPClient.Result
+    
+    init(stub: @escaping (URL) -> HTTPClient.Result) {
+      self.stub = stub
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+      completion(stub(url))
+      return Task()
+    }
+    
+    static var offline: HTTPClientStub {
+      HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
+    }
+    
+    static func online(_ stub: @escaping (URL) -> (HTTPURLResponse, Data)) -> HTTPClientStub {
+      HTTPClientStub { url in .success(stub(url)) }
+    }
+  }
+  
+  private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
+    private var feedCache: CachedFeed?
+    private var feedImageDataCache: [URL: Data] = [:]
+    
+    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+      feedCache = nil
+      completion(nil)
+    }
+    
+    func insert(items feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+      feedCache = CachedFeed(feed: feed, timestamp: timestamp)
+      completion(.success(()))
+    }
+    
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+      completion(.success(feedCache))
+    }
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+      feedImageDataCache[url] = data
+      completion(.success(()))
+    }
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+      completion(.success(feedImageDataCache[url]))
+    }
+    
+    static var empty: InMemoryFeedStore {
+      InMemoryFeedStore()
+    }
+  }
+  
+  private func response(for url: URL) -> (HTTPURLResponse, Data) {
+    let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+    return (response, makeData(for: url))
+  }
+  
+  private func makeData(for url: URL) -> Data {
+    switch url.absoluteString {
+    case "http://image.com":
+      return makeImageData()
+      
+    default:
+      return makeFeedData()
+    }
+  }
+  
+  private func makeImageData() -> Data {
+    return UIImage.make(withColor: .red).pngData()!
+  }
+  
+  private func makeFeedData() -> Data {
+    return try! JSONSerialization.data(withJSONObject: ["items": [
+      ["id": UUID().uuidString, "image": "http://image.com"],
+      ["id": UUID().uuidString, "image": "http://image.com"]
+    ]])
+  }
+
+}
