0e84e9083cb6a4e6b8c10a22b45f2b89a84e844b
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-03-29 09:48:37 +0800

    add emptyFeed snapshot

---
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -0,0 +1,55 @@
+//
+//  FeedSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Shengjun Xia on 2024/3/29.
+//
+
+import XCTest
+import EssentialFeediOS
+
+final class FeedSnapshotTests: XCTestCase {
+  
+  func test_emptyFeed() {
+    let sut = makeSUT()
+    sut.display(cellControllers: emptyFeed())
+    record(snapshot: sut.snapshot(), name: "EmptyFeed")
+  }
+  
+  private func makeSUT() -> FeedViewController {
+    let bundle = Bundle(for: FeedViewController.self)
+    let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+    let controller = storyboard.instantiateInitialViewController() as! FeedViewController
+    controller.loadViewIfNeeded()
+    return controller
+  }
+  
+  private func emptyFeed() -> [FeedImageCellController] {
+    []
+  }
+  
+  //#file means current file
+  private func record(snapshot: UIImage, name: String, file: StaticString = #file, line: UInt = #line) {
+    guard let snapshotData = snapshot.pngData() else {
+      XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+      return
+    }
+    
+    let snapshotURL = URL(fileURLWithPath: String(describing: file)).deletingLastPathComponent().appendingPathComponent("snapshots").appendingPathComponent("\(name).png")
+    do {
+      try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+      try snapshotData.write(to: snapshotURL)
+    } catch {
+      XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+    }
+  }
+}
+
+extension UIViewController {
+  func snapshot() -> UIImage {
+    let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+    return renderer.image { action in
+      view.layer.render(in: action.cgContext)
+    }
+  }
+}
