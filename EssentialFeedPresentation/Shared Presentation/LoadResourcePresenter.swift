7877c470b69a749936aeade051ca6e5503ab6113
Shengjun Xia <shengjun.xia@grabtaxi.com>
2024-04-05 19:18:10 +0800

    Remove title cuz it's specific logic

--- EssentialFeedPresentation/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeedPresentation/Shared Presentation/LoadResourcePresenter.swift
@@ -18,9 +18,6 @@ public class LoadResourcePresenter {
                       bundle: Bundle(for: FeedPresenter.self),
                       comment: "Error message displayed when we can't load the image feed from the server")
   }
-  public static var title: String {
-    NSLocalizedString("FEED_VIEW_TITLE", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Title for the feed view")
-  }
   
   public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
     self.feedView = feedView
